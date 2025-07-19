aa8b254f49fb740a13eb1e947f919bda2df0c2c8
unknown <semwanzamiraji@gmail.com>
2025-07-19 14:09:50 +0300

    addsomess

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI"}
+# Serve static files (CSS, JS, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/about")
-def about_me():
-    return {
+# Set up templates directory
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Home"})
+
+@app.get("/about", response_class=HTMLResponse)
+def about_me(request: Request):
+    context = {
+        "request": request,
+        "title": "About Me",
         "name": "Miraji Semwanza",
         "profession": "Computer Engineering Student / Software Developer",
         "focus": "Working on FastAPI project for web development",
         "location": "Tanzania",
         "goal": "Learning to build scalable backend APIs with Python FastAPI"
     }
+    return templates.TemplateResponse("about.html", context)
